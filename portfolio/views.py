2efdbccdedfe9226730c762b07d7d0da2250a01e
mmacranderuno <mmacrander@unomaha.edu>
2021-02-21 22:59:38 -0600

    A2

--- portfolio/views.py
+++ portfolio/views.py
@@ -50,8 +50,8 @@ def customer_edit(request, pk):
 def customer_delete(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
     customer.deleted = 1
-    stocks = Stock.objects.filter(customer=customer.id)
-    investments = Investment.objects.filter(customer=customer.id)
+    stocks = customer.stocks.all()
+    investments = customer.investments.all()
     for stock in stocks:
         stock.deleted = 1
         stock.deleted_date = datetime.datetime.now()
